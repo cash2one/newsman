923db6eb128dfd58f71c765160346cc9b4e6472d
chengdujin <chengdujin@gmail.com>
2013-07-19 14:28:20 +0800

    moved database code in rss_parser here

--- scraper/database.py
+++ scraper/database.py
@@ -5,3 +5,78 @@ import sys
 reload(sys) 
 sys.setdefaultencoding('UTF-8')
 
+
+# Todos
+# break update_database into several shorter mthods
+def update_database(entries=None, language=None):
+    ''''''
+    print '>>> update_database'
+    if not entries:
+        return None
+    # collection was created by the feed
+    added_entries = []
+    col = Collection(db, language)
+    for entry in entries:
+        duplicated = col.find_one({'link': entry['link']})
+        if duplicated:
+            print 'Find a duplicate for %s' % entry['title']
+            continue
+        item = col.find_one({'title': entry['title']})
+        if not item:
+            # transcode the link
+            try:
+                random_code = random.random()
+                # create a proper name for url encoding
+                safe_category = (entry['category']).strip().replace(" ", "-")
+                transcoded_path, big_images = transcoder.transcode(entry['language'], entry['title'], entry[
+                                                                   'link'], '%s_%s_%s_%s_%f' % (entry['language'], safe_category, entry[feed], entry['updated'], random_code))
+                if not transcoded_path:
+                    raise Exception('cannot transcode %s' % entry['link'])
+                else:
+                    entry[
+                        'transcoded'] = 'None' if not transcoded_path else transcoded_path
+                    entry[
+                        'big_images'] = 'None' if not big_images else big_images
+                    if entry['image'] == 'None' and entry['big_images'] != 'None':
+                        entry['image'] = []
+                        bimage_max = 0, 0
+                        for bimage in entry['big_images']:
+                            bimage_current = transcoder.get_image_size(bimage)
+                            if bimage_current > bimage_max:
+                                thumbnail_relative_path = '%s.jpeg' % bimage
+                                if len(thumbnail_relative_path) > 200:
+                                    thumbnail_relative_path = thumbnail_relative_path[
+                                        -200:]
+                                try:
+                                    thumbnail_url = thumbnail.get(
+                                        bimage, thumbnail_relative_path)
+                                    entry['image'] = thumbnail_url
+                                    bimage_max = bimage_current
+                                except IOError as e:
+                                    entry['big_images'].remove(bimage)
+                    elif entry['image'] and isinstance(entry['image'], list):
+                        entry['image'] = entry['image'][0]
+                    # Google TTS
+                    # only for English, at present
+                    if entry['language'] == 'en':
+                        try:
+                            random_code = random.randint(0, 1000000000)
+                            tts_web_path = '%s%s_%s_%i.mp3' % (
+                                THUMBNAIL_WEB_DIR, entry['language'], entry['updated'], random_code)
+                            tts_local_path = '%s%s_%s_%i.mp3' % (
+                                THUMBNAIL_LOCAL_DIR, entry['language'], entry['updated'], random_code)
+                            tts_provider.google(
+                                entry['language'], entry['title'], tts_local_path)
+                            entry['mp3'] = tts_web_path
+                        except Exception as e:
+                            entry['mp3'] = "None"
+                    # then save to database
+                    entry_id = col.save(entry)
+                    entry['_id'] = str(entry_id)
+                    added_entries.append((entry, REDIS_ENTRY_EXPIRATION))
+                    print 'processed %s' % entry['title']
+            except Exception as e:
+                print str(e)
+        else:
+            print 'Find a duplicate for %s' % entry['title']
+    return added_entries
