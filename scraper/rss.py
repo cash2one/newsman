033429df39c6bcd8cc7d28213f12c3f63ef69da1
chengdujin <chengdujin@gmail.com>
2013-07-20 17:47:24 +0800

    added rss.py

---
+++ scraper/rss.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+# rss is the interface to rss parsing,
+# processing and storing.
+#
+# @author Jin Yuan
+# @email jinyuan@baidu.com
+# @created Jul. 20, 2013
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+
