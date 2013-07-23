9b1d1ce2a168239b14c06f34af36c102727121f1
chengdujin <chengdujin@gmail.com>
2013-07-23 14:43:34 +0800

    added test for rss.py

---
+++ tests/test_rss.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+sys.path.append('..')
+
+from scraper import rss
+
+#entries = rss_parser.parse(feed_link='http://news.yahoo.com/rss/us', language='en')
+#entries = rss_parser.parse(feed_link='http://www.engadget.com/rss.xml', language='en')
+#entries = rss_parser.parse(feed_link='http://rss.cnn.com/rss/edition_sport.rss', language='en')
+entries = rss.update(feed_link='http://news.yahoo.com/rss/sports', feed_id='007', language='en')
+
