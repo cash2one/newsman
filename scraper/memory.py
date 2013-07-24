fa07c514a99bb6fae1ecd8f55e996353939b2209
chengdujin <chengdujin@gmail.com>
2013-07-24 09:05:17 +0800

    refactored memory.update

--- scraper/memory.py
+++ scraper/memory.py
@@ -6,38 +6,35 @@ reload(sys)
 sys.setdefaultencoding('UTF-8')
 sys.path.append('..')
 
-
-from datetime import datetime
 from administration.config import rclient
-from administration.config import MEMORY_RESTORATION_DAYS
 from administration.config import MEMORY_EXPIRATION_DAYS
 
 
-def update(added_items=None, language=None, category=None, feed_id=None):
-    ''''''
-    # Todos
-    # add more comments
-    if not added_items:
+# Todos
+# - add more comments
+# - be precautious with possible redis adding failure
+def update(entries=None, language=None, categories=None):
+    """
+    add news and its attributes to memory
+    """
+    if not entries:
         return None
+    if not language or not categories:
+        raise Exception("ERROR: Method signature not well formed!")
+    
     # add entry objects to memory
-    for item in added_items:
-        entry = item[0]
-        expiration = item[1]
-        entry['_id'] = str(entry['_id'])
+    for entry in entries:
+        # add a piece of news into memory
         a = rclient.set(entry['_id'], entry)
-        # set expire time to 3 days later
+
+        # expired in redis is counted in seconds
+        expiration = MEMORY_EXPIRATION_DAYS * 24 * 60 * 60
         rclient.expire(entry['_id'], expiration)
 
         # add entry ids to the language list
-        b = rclient.zadd(language, entry['updated'], entry['_id'])
+        b = rclient.zadd(language, entry['updated_parsed'], entry['_id'])
         # print entry['_id'], 'is added to memory', rclient.zcard(language)
 
         # add entry ids to the category list
-        c = rclient.zadd('%s-%s' %
-                         (language, category), entry['updated'], entry['_id'])
-
-        # add entry ids to the feed list
-        d = rclient.zadd('%s-%s-%s' %
-                         (language, category, feed_id), entry['updated'], entry['_id'])
-        print datetime.utcfromtimestamp(entry['updated']), entry['title'], a, b, c, d
-    return len(added_items)
+        for category in categories: 
+            c = rclient.zadd('%s-%s' % (language, category), entry['updated_parsed'], entry['_id'])
