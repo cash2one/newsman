201d594eca990619f72066533e3881b767b52dd0
chengdujin <chengdujin@gmail.com>
2013-07-28 01:38:49 +0800

    updated constant var names

--- tests/test_tts_provider.py
+++ tests/test_tts_provider.py
@@ -1,10 +1,10 @@
 import random
 from config import MEDIA_LOCAL_DIR
-from config import MEDIA_WEB_DIR
+from config import MEDIA_PUBLIC_DIR
 import tts_provider
 
 random_code = random.randint(0, 1000000000)
-tts_web_path = '%s%s_%s.mp3' % (MEDIA_WEB_DIR, 'en', 'test')
+tts_web_path = '%s%s_%s.mp3' % (MEDIA_PUBLIC_DIR, 'en', 'test')
 tts_local_path = '%s%s_%s.mp3' % (MEDIA_LOCAL_DIR, 'en', 'test')
 print tts_local_path
 print tts_web_path
