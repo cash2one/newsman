93dd8e9fd7df5c2a585df5ffbc2fdd8904b4ba16
chengdujin <chengdujin@gmail.com>
2013-07-30 13:54:38 +0800

    rewrote feed.py

--- administration/feed.py
+++ administration/feed.py
@@ -12,9 +12,10 @@ reload(sys)
 sys.setdefaultencoding('UTF-8')
 sys.path.append("..")
 
+from administration import database as db_feed
 from BeautifulSoup import BeautifulStoneSoup
-import entry
 import feedparser
+from scraper import rss
 import task
 import time
 
@@ -22,17 +23,10 @@ from administration.config import FEED_REGISTRAR
 from administration.config import LANGUAGES
 
 
-def create_language_collection(feed_info=None):
-    ''''''
-    if not feed_info:
-        return None
-    if feed_info['language'] not in db.collection_names():
-        feed_id = db.create_collection(feed_info['language'])
-        return feed_id
-    return None
-
-
 def _read_source(d=None, feed_link=None, language=None, categories=None):
+    """
+    parse the feed for meta information
+    """
     if 'feed' in d:
         feed_info = {}
         # read in passed values
@@ -42,11 +36,13 @@ def _read_source(d=None, feed_link=None, language=None, categories=None):
 
         if 'title' in d.feed:
             feed_info['feed_title'] = d.feed.title.strip()
+        if 'status' in d:
+            feed_info['status'] = d.status
         if 'rights' in d.feed:
             feed_info['rights'] = d.feed.rights.strip()
-        if 'etag' in d.feed:
+        if 'etag' in d:
             feed_info['etag'] = d.feed.etag.strip()
-        if 'modified' in d.feed:
+        if 'modified' in d:
             feed_info['modified'] = d.feed.modified.strip()
         return feed_info
     else:
@@ -71,19 +67,9 @@ def add(feed_link=None, language=None, categories=None):
     if d:
         # feed level
         feed_info = _read_source(d, feed_link, language, categories)
-        _register_source(feed_info)
-        feed_id = create_language_collection(feed_info)
-        print '1/4 .. created entry in database'
-        # ToDos
-        # add feed_id checking procedure
-
+        feed_id = db_feed.insert_feed(feed_info)
         # add entries of this feed
-        entry.add_entries(feed_id, feed_info['feed_link'], feed_info['language'], feed_info['categories']) 
-        print '2/4 .. added all rss entries'
-
-        # add to update list
-        task.add_task(feed_id, feed_info['feed_link'], feed_info['language'], feed_info['categories'])
-        print '4/4 .. added to task list'
-        return 0
+        rss.update(str(feed_id), feed_info['feed_link'], feed_info['language'], feed_info['categories']) 
     else:
-        return 4
+        raise Exception("ERROR: RSS source %s cannot be interpreted!" % feed_link)
+        
