e1a43c7700dc900e5f4f187643097d21b0434c12
chengdujin <chengdujin@gmail.com>
2013-07-30 15:56:23 +0800

    added feed2db.py

---
+++ alert_maintenance/feed2db.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+# feed2db works to turn text-based feed list into database
+#
+# @author Jin Yuan
+# @contact jinyuan@baidu.com
+# @created Jul. 30, 2013
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+
