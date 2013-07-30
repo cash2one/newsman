91ddf88a334168f14615e3f68021dee19a7c83ea
chengdujin <chengdujin@gmail.com>
2013-07-30 16:11:54 +0800

    added some imports

--- alert_maintenance/feed2db.py
+++ alert_maintenance/feed2db.py
@@ -10,15 +10,34 @@
 import sys 
 reload(sys) 
 sys.setdefaultencoding('UTF-8')
+sys.path.append('..')
+
+from administration.config import Collection
+from administration.config import db
+from administration.config import FEED_REGISTRAR
+
+file_suffix = '/home/jinyuan/Downloads/global-mobile-news/alert_maintenance/maintenance/'
 
 
-# TODO: add docs
 def _parse_task(line):
     """
-    docs needed!
+    read *_feeds_list.txt
     """
     if line:
         task = line.split('*|*')
         return task[0], task[1], task[2], task[3]
     else:
         return None
+
+
+def _convert(language='en'):
+    """
+    turn text-based feed infor into database items
+    """
+    feeds_list = open('%s%s_feeds_list.txt' % (file_suffix, language), 'r')
+    lines = feeds_list.readlines()
+    feeds_list.close()
+
+    for line in lines:
+        if line.strip():
+            language, category, feed_title, feed_link = _parse_task(line)
