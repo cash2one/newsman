eae44b79bfb571f52927b2add7385258732ea1d0
chengdujin <chengdujin@gmail.com>
2013-08-06 02:41:39 +0800

    added test file for uck

---
+++ tests/test_baidu_uck.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+sys.path.append("..")
+
+from data_processor import baidu_uck
+
+def _main(url):
+    baidu_uck.convert(url)
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    _main(url)
+
