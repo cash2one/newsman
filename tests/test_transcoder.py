130a7c55110621cd360b0d856a408e3f46544cf3
chengdujin <chengdujin@gmail.com>
2013-08-06 02:17:10 +0800

    added a print

--- tests/test_transcoder.py
+++ tests/test_transcoder.py
@@ -8,4 +8,4 @@ sys.path.append("..")
 
 from data_processor import transcoder
 
-transcoder.convert('en', '旅情そそるクルーズ列車大ヒット　ＪＲ九州「ななつ星」来春まで満席　東西も追随へ', 'http://sankei.jp.msn.com/economy/news/130803/biz13080321140003-n1.htm', 'chengdujin', 'test')
+print transcoder.convert('en', '旅情そそるクルーズ列車大ヒット　ＪＲ九州「ななつ星」来春まで満席　東西も追随へ', 'http://sankei.jp.msn.com/economy/news/130803/biz13080321140003-n1.htm', 'chengdujin', 'test')
