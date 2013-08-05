efc16b30fecc38d64eefc8cd430a94f50328ca63
chengdujin <chengdujin@gmail.com>
2013-08-06 01:56:31 +0800

    finished _prepare_link

--- data_processor/burify.py
+++ data_processor/burify.py
@@ -13,6 +13,25 @@ reload(sys)
 sys.setdefaultencoding('UTF-8')
 sys.path.append('..')
 
+import chardet
+from readability import Document
+import urllib2
+
+
+def _prepare_link(url):
+    """
+    decode with the correct encoding
+    """
+    html = urllib2.urlopen(url).read()
+    if html:
+        detected = chardet.detect(html)
+        if detected:
+            data = html.decode(detected['encoding'])
+        else:
+            data = html.decode('utf-8')
+    else:
+        raise Exception("ERROR: Cannot read %s" % url)
+
 
 def convert(link):
     """
