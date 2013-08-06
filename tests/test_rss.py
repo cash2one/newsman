a1815a5be23b2a307e43ad736d25f4e9d3452b45
chengdujin <chengdujin@gmail.com>
2013-08-06 14:37:33 +0800

    replace feed_id with a real one

--- tests/test_rss.py
+++ tests/test_rss.py
@@ -12,11 +12,11 @@ from scraper import rss
 #entries = rss_parser.parse(feed_link='http://www.engadget.com/rss.xml', language='en')
 #entries = rss_parser.parse(feed_link='http://rss.cnn.com/rss/edition_sport.rss', language='en')
 categories = ['Sports']
-entries = rss.update(feed_link='http://news.yahoo.com/rss/sports', feed_id='007', language='en', categories=categories)
+entries = rss.update(feed_link='http://news.yahoo.com/rss/sports', feed_id='5200990d1d41c813b222c2c5', language='en', categories=categories, transcoder_type='readability')
 #categories = ['Technology', 'Internet']
 #entries = rss.update(feed_link='http://www.engadget.com/rss.xml', feed_id='008', language='en', categories=categories)
-categories = ['BBC', 'News']
-entries = rss.update(feed_link='http://feeds.bbci.co.uk/news/world/rss.xml', feed_id='008', language='en', categories=categories)
+#categories = ['BBC', 'News']
+#entries = rss.update(feed_link='http://feeds.bbci.co.uk/news/world/rss.xml', feed_id='008', language='en', categories=categories)
 
 #entries = rss.update(feed_link='http://www.antaranews.com/rss/nasional-kesehatan', feed_id='008', language='ind', categories=categories)
 
