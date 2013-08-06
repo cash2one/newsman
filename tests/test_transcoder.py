ed6dfa70e4a005c6783c1db34b10892cdebb9602
chengdujin <chengdujin@gmail.com>
2013-08-06 14:03:19 +0800

    support indicating language in testing

--- tests/test_transcoder.py
+++ tests/test_transcoder.py
@@ -8,8 +8,8 @@ sys.path.append("..")
 
 from data_processor import transcoder
 
-def _main(url, trans):
-    print transcoder.convert('en', '123 test', url, trans, 'test')
+def _main(language, url, trans):
+    print transcoder.convert(language, '123 test', url, trans, 'test')
 
 if __name__ == "__main__":
     url = sys.argv[1]
@@ -17,5 +17,8 @@ if __name__ == "__main__":
     trans = "chengdujin"
     if len(sys.argv) > 2:
         trans = sys.argv[2]
-    _main(url, trans)
+    language = 'en'
+    if len(sys.argv) > 3:
+        language = sys.argv[3]
+    _main(language, url, trans)
 
