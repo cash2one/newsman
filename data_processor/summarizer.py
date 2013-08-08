3f9662193b72befa230c7637bcc87a8d13627243
chengdujin <chengdujin@gmail.com>
2013-08-09 01:42:25 +0800

    modified summarizer

--- data_processor/summarizer.py
+++ data_processor/summarizer.py
@@ -35,7 +35,7 @@ def _get_shorter_text(content, language, limit):
     # break text by sentence
     if language.startswith('zh') or language == 'ja':
         jp_sent_tokenizer = nltk.RegexpTokenizer(u'[^!?.！？。．]*[!?.！？。]*')
-        sentences = jp_sent_tokenizer.tokenize(query)
+        sentences = jp_sent_tokenizer.tokenize(content)
     else: # supports latin-based, thai and arabic
         sentences = nltk.sent_tokenize(content)
 
