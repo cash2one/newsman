fe2ba6dc563eb1619fc90d88ae17a03ace5974ad
chengdujin <chengdujin@gmail.com>
2013-08-09 00:56:33 +0800

    updated the test file

---
+++ tests/test_summarizer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+sys.path.append("..")
+
+from administration.config import Collection
+from administration.config import db
+from data_processor import summarizer
+
+
+def main():
+    col = Collection(db, 'en')
+    items = col.find({}, {'summary':1, 'transcoded':1})
+    if items:
+        for item in items:
+            summary = summarizer.extract(item['summary'], None, 'en')
+            print item['_id'], len(summary)
+            print summary
+            print
+
+
+if __name__ == '__main__':
+    main()
