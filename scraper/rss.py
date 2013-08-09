90b3560b7d770495f3c93c45be7560b368a96272
chengdujin <chengdujin@gmail.com>
2013-08-09 11:43:37 +0800

    fixed a small-size image cannot generate hot news mage

--- scraper/rss.py
+++ scraper/rss.py
@@ -53,6 +53,7 @@ def _value_added_process(entries=None, language=None, transcoder_type='chengduji
     for entry in entries:
         # get a random int from 100 million possibilities
         try:
+            print '  working on transcoding ...'
             # [MUST-HAVE] transcoding
             rand = random.randint(0, 100000000)
             transcoded_relative_path = '%s_%s_%s_%i' % (entry['language'], entry['feed_id'], entry['updated'], rand)
@@ -88,15 +89,17 @@ def _value_added_process(entries=None, language=None, transcoder_type='chengduji
                         hot_web, hot_local = image_helper.scale_image(image=biggest, image_data=image_data, size_expected=HOT_IMAGE_SIZE, resize_by_width=True, crop_by_center=False, relative_path='%s_hotnews' % image_relative_path)
                         entry['hot_news_image'] = hot_web if hot_web else None
                         entry['hot_news_image_local'] = hot_local if hot_local else None
+
                         # category image
-                            
                         category_web, category_local = image_helper.scale_image(image=biggest, image_data=image_data_category, size_expected=CATEGORY_IMAGE_SIZE, resize_by_width=True, crop_by_center=False, relative_path='%s_category' % image_relative_path)
                         entry['category_image'] = category_web if category_web else None
                         entry['category_image_local'] = category_local if category_local else None
+
                         # new item thumbnail image
                         thumbnail_web, thumbnail_local = image_helper.scale_image(image=biggest, image_data=image_data_thumbnail, size_expected=THUMBNAIL_IMAGE_SIZE, resize_by_width=True, crop_by_center=True, relative_path='%s_thumbnail' % image_relative_path)
                         entry['thumbnail_image'] = thumbnail_web if thumbnail_web else None
                         entry['thumbnail_image_local'] = thumbnail_local if thumbnail_local else None 
+
                         # for older version users
                         entry['image'] = entry['thumbnail_image']['url'] if thumbnail_web else None
                     except IOError as k:
