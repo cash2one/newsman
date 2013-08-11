e6d6155ef820eb6fe7beb2bb6a0c388b1fb7ef61
chengdujin <chengdujin@gmail.com>
2013-08-11 15:46:32 +0800

    autopep8ed

--- tests/test_transcoder/transcoder_web.py
+++ tests/test_transcoder/transcoder_web.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python 
-#-*- coding: utf-8 -*- 
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 
 # transcoder_web provides a web interface for three transcoders
 #
@@ -8,8 +8,8 @@
 # @created Aug. 10, 2013
 
 
-import sys 
-reload(sys) 
+import sys
+reload(sys)
 sys.setdefaultencoding('UTF-8')
 sys.path.append('../..')
 
@@ -23,18 +23,21 @@ urls = (
 
 
 class Transcoders:
+
     def GET(self):
         data = web.input(url="this should not be seen", language='no language')
         return self._transcode(data.language, data.url)
 
-    
     def _transcode(self, language, url):
         """
         call each transcoder
         """
-        title_simplr, content_simplr = transcoder.convert(language=language, link=url, transcoder='chengdujin', stdout=True)
-        title_burify, content_burify = transcoder.convert(language=language, link=url, transcoder='readability', stdout=True)
-        title_uck, content_uck = transcoder.convert(language=language, link=url, transcoder='uck', stdout=True)
+        title_simplr, content_simplr = transcoder.convert(
+            language=language, link=url, transcoder='chengdujin', stdout=True)
+        title_burify, content_burify = transcoder.convert(
+            language=language, link=url, transcoder='readability', stdout=True)
+        title_uck, content_uck = transcoder.convert(
+            language=language, link=url, transcoder='uck', stdout=True)
         return render.combiner(title_simplr, content_simplr, title_burify, content_burify, title_uck, content_uck, 'Original Webpage')
 
 
