b4faef66e60f397d9a2e8ccc68c6452d77c022ea
chengdujin <chengdujin@gmail.com>
2013-08-11 15:45:42 +0800

    autopep8ed

--- scraper/rss.py
+++ scraper/rss.py
@@ -54,21 +54,26 @@ def _value_added_process(entries=None, language=None, transcoder_type='chengduji
         try:
             # [MUST-HAVE] transcoding
             rand = random.randint(0, 100000000)
-            transcoded_relative_path = '%s_%s_%s_%i' % (entry['language'], entry['feed_id'], entry['updated'], rand)
+            transcoded_relative_path = '%s_%s_%s_%i' % (
+                entry['language'], entry['feed_id'], entry['updated'], rand)
             # high chances transcoder cannot work properly
-            entry['transcoded'], entry['transcoded_local'], raw_transcoded_content, images_from_transcoded = transcoder.convert(entry['language'], entry['title'], entry['link'], transcoder_type, transcoded_relative_path)
+            entry['transcoded'], entry['transcoded_local'], raw_transcoded_content, images_from_transcoded = transcoder.convert(
+                entry['language'], entry['title'], entry['link'], transcoder_type, transcoded_relative_path)
 
             # [MUST-HAVE] summary
-            entry['summary'] = summarizer.extract(entry['summary'], raw_transcoded_content, entry['language'])
+            entry['summary'] = summarizer.extract(
+                entry['summary'], raw_transcoded_content, entry['language'])
 
             # process images found in the transcoded data
             if images_from_transcoded:
                 # images from transcoded are already normalized
                 entry['images'].extend(images_from_transcoded)
                 # remove duplicated images
-                entry['images'] = image_helper.dedupe_images(entry['images']) if entry.has_key('images') and entry['images'] else None
+                entry['images'] = image_helper.dedupe_images(
+                    entry['images']) if entry.has_key('images') and entry['images'] else None
             # make images none if nothing's there, instead of a []
-            entry['images'] = entry['images'] if entry.has_key('images') and entry['images'] else None
+            entry['images'] = entry['images'] if entry.has_key(
+                'images') and entry['images'] else None
 
             # [OPTIONAL] generate 3 types of images: thumbnail, category image and hot news image
             if entry.has_key('images') and entry['images']:
@@ -76,28 +81,40 @@ def _value_added_process(entries=None, language=None, transcoder_type='chengduji
                 if biggest:
                     try:
                         rand = random.randint(0, 100000000)
-                        image_relative_path = '%s_%s_%s_%i' % (entry['language'], entry['feed_id'], entry['updated'], rand)
+                        image_relative_path = '%s_%s_%s_%i' % (
+                            entry['language'], entry['feed_id'], entry['updated'], rand)
 
                         # hot news image
-                        hot_web, hot_local = image_helper.scale_image(image=biggest, size_expected=HOT_IMAGE_SIZE, resize_by_width=True, crop_by_center=False, relative_path='%s_hotnews' % image_relative_path)
+                        hot_web, hot_local = image_helper.scale_image(
+                            image=biggest, size_expected=HOT_IMAGE_SIZE, resize_by_width=True, crop_by_center=False, relative_path='%s_hotnews' % image_relative_path)
                         entry['hot_news_image'] = hot_web if hot_web else None
-                        entry['hot_news_image_local'] = hot_local if hot_local else None
+                        entry[
+                            'hot_news_image_local'] = hot_local if hot_local else None
 
                         # category image
-                        category_web, category_local = image_helper.scale_image(image=biggest, size_expected=CATEGORY_IMAGE_SIZE, resize_by_width=True, crop_by_center=False, relative_path='%s_category' % image_relative_path)
-                        entry['category_image'] = category_web if category_web else None
-                        entry['category_image_local'] = category_local if category_local else None
+                        category_web, category_local = image_helper.scale_image(
+                            image=biggest, size_expected=CATEGORY_IMAGE_SIZE, resize_by_width=True, crop_by_center=False, relative_path='%s_category' % image_relative_path)
+                        entry[
+                            'category_image'] = category_web if category_web else None
+                        entry[
+                            'category_image_local'] = category_local if category_local else None
 
                         # thumbnail image
-                        if float(biggest['width']) / float(biggest['height'])  >= THUMBNAIL_STYLE: # thumbnail_landscape_size
-                            thumbnail_web, thumbnail_local = image_helper.scale_image(image=biggest, size_expected=THUMBNAIL_LANDSCAPE_SIZE, resize_by_width=True, crop_by_center=False, relative_path='%s_thumbnail' % image_relative_path)
-                        else: # else thumbnail_portrait_size
-                            thumbnail_web, thumbnail_local = image_helper.scale_image(image=biggest, size_expected=THUMBNAIL_PORTRAIT_SIZE, resize_by_width=True, crop_by_center=False, relative_path='%s_thumbnail' % image_relative_path)
-                        entry['thumbnail_image'] = thumbnail_web if thumbnail_web else None
-                        entry['thumbnail_image_local'] = thumbnail_local if thumbnail_local else None 
+                        # thumbnail_landscape_size
+                        if float(biggest['width']) / float(biggest['height']) >= THUMBNAIL_STYLE:
+                            thumbnail_web, thumbnail_local = image_helper.scale_image(
+                                image=biggest, size_expected=THUMBNAIL_LANDSCAPE_SIZE, resize_by_width=True, crop_by_center=False, relative_path='%s_thumbnail' % image_relative_path)
+                        else:  # else thumbnail_portrait_size
+                            thumbnail_web, thumbnail_local = image_helper.scale_image(
+                                image=biggest, size_expected=THUMBNAIL_PORTRAIT_SIZE, resize_by_width=True, crop_by_center=False, relative_path='%s_thumbnail' % image_relative_path)
+                        entry[
+                            'thumbnail_image'] = thumbnail_web if thumbnail_web else None
+                        entry[
+                            'thumbnail_image_local'] = thumbnail_local if thumbnail_local else None
 
                         # for older version users
-                        entry['image'] = entry['thumbnail_image']['url'] if thumbnail_web else None
+                        entry['image'] = entry['thumbnail_image'][
+                            'url'] if thumbnail_web else None
                     except IOError as k:
                         entry['error'].append(str(k) + '\n')
 
@@ -150,13 +167,13 @@ def update(feed_link=None, feed_id=None, language=None, categories=None, transco
     Note. categories are ids of category item
     Notel categories are kept for manual testing
     """
-    if not feed_id and not (feed_link and language) :
+    if not feed_id and not (feed_link and language):
         raise Exception(
             "ERROR: Method signature not well formed!")
 
     if feed_id:
         feed = db_feeds.get(feed_id=feed_id)
-    else: 
+    else:
         feed = db_feeds.get(feed_link=feed_link, language=language)
 
     feed_id = str(feed['_id']) if feed else feed_id
@@ -183,7 +200,8 @@ def update(feed_link=None, feed_id=None, language=None, categories=None, transco
     entries = db_news.update(entries, language)
     # and some data, like feed_title, etag and modified to db_feeds
     # only feed_id is necessary, others are optional **kwargs
-    db_feeds.update(feed_id=feed_id, status=status_new, feed_title=feed_title_new, etag=etag_new, modified=modified_new)
+    db_feeds.update(feed_id=feed_id, status=status_new,
+                    feed_title=feed_title_new, etag=etag_new, modified=modified_new)
 
     # store in memory
     memory.update(entries, language, categories)
