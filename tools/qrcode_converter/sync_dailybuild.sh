b16601dcd1d16473f5897825fc95200f5fdecf51
chengdujin <chengdujin@gmail.com>
2013-08-12 18:19:30 +0800

    updated qrcode crontab script

--- tools/qrcode_converter/sync_dailybuild.sh
+++ tools/qrcode_converter/sync_dailybuild.sh
@@ -4,8 +4,7 @@
 # Replicate file trees to server using rsync
 # or call from cron (make sure ssh key is loaded beforehand)
 
-cd home
-rsync -e ssh -av --delete \
- work/web/doremi.baidu.com/hao123gm/global-new_dailybuild/ \
- work@hk01-gmobile-web00.vm: \
- /home/work/web/doremi.baidu.com/hao123/global-new_dailybuild
+cd /home
+rsync -e ssh -av --delete work/web/doremi.baidu.com/hao123gm/\
+    globalnews_dailybuild/ work@hk01-gmobile-web00.vm:\
+    /home/work/web/doremi.baidu.com/hao123gm/globalnews_dailybuild
