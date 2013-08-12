2f10cb2a7a68b7eb6b122e642b7100c180881dfb
chengdujin <chengdujin@gmail.com>
2013-08-12 13:45:28 +0800

    updated memory.py according to pylints suggestions

--- scraper/memory.py
+++ scraper/memory.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+"""
+memory provides an interface to store news in the memory/redis
+"""
+# @author chengdujin
+# @contact chengdujin@gmail.com
+# @created Jul. 2013
+
+
 import sys
 reload(sys)
 sys.setdefaultencoding('UTF-8')
 sys.path.append('..')
 
 from config import rclient
+
+# CONSTANTS
 from config import MEMORY_EXPIRATION_DAYS
 
 
@@ -25,17 +35,17 @@ def update(entries=None, language=None, categories=None):
     # add entry objects to memory
     for entry in entries:
         # add a piece of news into memory
-        a = rclient.set(entry['_id'], entry)
+        rclient.set(entry['_id'], entry)
 
         # expired in redis is counted in seconds
         expiration = MEMORY_EXPIRATION_DAYS * 24 * 60 * 60
         rclient.expire(entry['_id'], expiration)
 
         # add entry ids to the language list
-        b = rclient.zadd("news::%s" % language, entry['updated'], entry['_id'])
+        rclient.zadd("news::%s" % language, entry['updated'], entry['_id'])
         # print entry['_id'], 'is added to memory', rclient.zcard(language)
 
         # add entry ids to the category list
         for category in categories:
-            c = rclient.zadd('news::%s::%s' %
-                             (language, category), entry['updated'], entry['_id'])
+            rclient.zadd('news::%s::%s' % (language, category), \
+                    entry['updated'], entry['_id'])
