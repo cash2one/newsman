f7efa8e6313a51bb3464ee7b0ea9330e8cb1e1f2
chengdujin <chengdujin@gmail.com>
2013-08-12 14:22:02 +0800

    autopep8ed and manually changed line length

--- tools/qrcode_converter/qrcode_converter.py
+++ tools/qrcode_converter/qrcode_converter.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python 
-#-*- coding: utf-8 -*- 
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 
-# qrcode_converter generates a qrcode from users input
-#
-# @author Jin Yuan
-# @contact jinyuan@baidu.com
+"""
+qrcode_converter generates a qrcode from users input
+"""
+# @author chengdujin
+# @contact chengdujin@gmail.com
 # @created Aug. 8, 2013
 
-import sys 
-reload(sys) 
+
+import sys
+reload(sys)
 sys.setdefaultencoding('UTF-8')
 
 import qrcode
@@ -18,7 +20,9 @@ def _convert(data, output):
     """
     well, convert the data to a jpeg-based image
     """
-    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4,)
+    qr = qrcode.QRCode(version=1, \
+            error_correction=qrcode.constants.ERROR_CORRECT_L, \
+            box_size=10, border=4,)
     qr.add_data(data)
     qr.make(fit=True)
 
@@ -27,12 +31,12 @@ def _convert(data, output):
     img.save(output, 'PNG')
     # for debugging
     print 'QR image saved to %s!' % output
-    
-    
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         raise Exception('ERROR: Method not well formed!')
-    
+
     data = sys.argv[1]
     output = "%s%s" % (sys.argv[2], sys.argv[3])
     _convert(data, output)
