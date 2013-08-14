ccdc56def9a830e766c1d5b3118f855b2235ad18
chengdujin <chengdujin@gmail.com>
2013-08-14 23:42:12 +0800

    added scrape.py

---
+++ watchdog/scrape.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+"""
+scrape is a task to scrape rss resources
+"""
+# @author chengdujin
+# @contact chengdujin@gmail.com
+# @created Aug. 14, 2013
+
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+
