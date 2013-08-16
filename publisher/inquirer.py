202353137fb156ab85f854a9022c4cf4a928e6a8
chengdujin <chengdujin@gmail.com>
2013-08-16 16:03:56 +0800

    changed search_limit to 30

--- publisher/inquirer.py
+++ publisher/inquirer.py
@@ -44,7 +44,7 @@ def get_categories_by_language(language=None):
     """
     if not language:
         return None
-    search_limit = 50
+    search_limit = 30
     images_limit = 5
 
     category_images = {}
