a28d9662e64577bbb80acd2097b0b99098a5f0ef
chengdujin <chengdujin@gmail.com>
2013-08-22 15:30:30 +0800

    added clean_disk

---
+++ watchdog/clean_disk.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+"""
+daily, clean saved expired files, temporary files and unrecorded files on disk
+"""
+# @author chengudjin
+# @contact chengdujin@gmail.com
+# @created Aug. 22, 2013
+
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+
