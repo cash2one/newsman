6b49925a28af8b08148445a983d2e780ed85715b
chengdujin <chengdujin@gmail.com>
2013-08-23 01:12:03 +0800

    initialized restore.py

---
+++ watchdog/baks/restore.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+"""
+restore memory from database, if memory failed
+"""
+# @author chengdujin
+# @contact chengdujin@gmail.com
+# @created Aug. 23, 2013
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+
