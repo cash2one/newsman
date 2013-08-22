97215f23986b8f9d5bd2b6ec1a0c4ffd1e9a4d7c
chengdujin <chengdujin@gmail.com>
2013-08-23 00:07:40 +0800

    finished writting clean_process.py

--- watchdog/clean_process.py
+++ watchdog/clean_process.py
@@ -13,3 +13,17 @@ import sys
 reload(sys) 
 sys.setdefaultencoding('UTF-8')
 
+import subprocess
+
+
+def clean():
+    """
+    kill zombie processes if there is any
+    """
+    command = "kill -HUP `ps -A -ostat,ppid | grep -e '^[Zz]' | awk '{print $2}'`"
+    subprocess.Popen(command, stderr=subprocess.PIPE, shell=True)
+
+
+if __name__ == '__main__':
+    clean()
+
