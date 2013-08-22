e30513771aab242b77cb326d8856f8b7193566bb
chengdujin <chengdujin@gmail.com>
2013-08-22 15:27:47 +0800

    added clean_memory

---
+++ watchdog/clean_memory.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+"""
+daily work, clean expired items and its place in queues in memory
+"""
+# @author chengdujin
+# @contact chengdujin@gmail.com
+# @created Aug. 22, 2013
+
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+
