c33a40239579b97014ac3408159dc771dbe55bf6
chengdujin <chengdujin@gmail.com>
2013-08-22 18:55:27 +0800

    added clean_process to remove zombie processes on linux

---
+++ watchdog/clean_process.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+"""
+clean zombie processes
+"""
+# @author chengdujin
+# @contact chengdujin@gmail.com
+# @created Aug. 22, 2013
+
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+
