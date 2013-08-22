a86acc0fc80bfc894d5cdff7be722dc7f746eabc
chengdujin <chengdujin@gmail.com>
2013-08-22 12:30:46 +0800

    updated fabfile

--- tools/fabfile.py
+++ tools/fabfile.py
@@ -15,9 +15,10 @@ reload(sys)
 sys.setdefaultencoding('UTF-8')
 
 from fabric.api import *
+import os
 
 # CONSTANTS
-env.GIT_REPO_URL = 'https://github.com/chengdujin/bgm_news'
+env.GIT_REPO_URL = 'https://github.com/chengdujin/ubuntu-essentials'
 
 
 def local ():
@@ -26,7 +27,10 @@ def local ():
     env.servername = 'local'
 
 
-def staging():
+def aws():
+    """
+    set up basic settings for aws connectoins
+    """
     env.user = 'ubuntu'
     env.hosts = ['54.251.107.116', '54.232.81.44', '54.248.227.71']
     env.key_filename = ['/home/jinyuan/Public/AWS_identifiers/mandy.pem', '/home/jinyuan/Public/AWS_identifiers/guochen.pem', '/home/jinyuan/Public/AWS_identifiers/searchet.pem']
@@ -34,8 +38,21 @@ def staging():
     env.PIP_REQUIREMENTS_PATH = '%s/requirements.txt' % env.REMOTE_CODEBASE_PATH
     env.servername = ['aws_sing', 'aws_sao', 'aws_tokyo']
 
-def host_type():
-    run('uname -s')
+# ==============================================
+# Setup setting
+# ==============================================
+def setup_repo ():
+    """
+    git clone from repo in github. Need to add public key to github server.
+    """
+    print '=== CLONE FROM GITHUB ==='
+    with cd(os.path.dirname(env.REMOTE_CODEBASE_PATH)):
+        run("git clone %s" % (env.GIT_REPO_URL))
+        
 
 def ll():
-    run('ls -lhF')
+    print '=== LISTING ==='
+    run('ls -lFh')
+        
+        
+        
