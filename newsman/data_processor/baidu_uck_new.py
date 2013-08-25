cdc6ce6cc9849f3a5d3359ecebdf28d4efaec12c
chengdujin <chengdujin@gmail.com>
2013-08-25 11:37:33 +0800

    added baidu_uck_new.py and initialized it

---
+++ newsman/data_processor/baidu_uck_new.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+"""
+A wrapper for UCK's new interface
+"""
+# @author chengdujin
+# @contact chengudjin@gmail.com
+# @created Aug. 25, 2013
+
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+sys.path.append("..")
