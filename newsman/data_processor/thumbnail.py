9ce1e1b0e833081b4e7eed6f66300799628741f0
chengdujin <chengdujin@gmail.com>
2013-08-26 22:28:54 +0800

    added logging to thumbnail.py and autopep8ed

--- newsman/data_processor/thumbnail.py
+++ newsman/data_processor/thumbnail.py
@@ -14,6 +14,7 @@ reload(sys)
 sys.setdefaultencoding('UTF-8')
 sys.path.append('..')
 
+from config import logging
 from cStringIO import StringIO
 import Image
 import urllib2
@@ -25,67 +26,88 @@ from config import IMAGES_PUBLIC_DIR
 from config import UCK_TIMEOUT
 
 
-# TODO: boundary checker should not return None, instead probably an Exception
 # TODO: this method should be moved to image_helper
 def is_valid_image(image_url):
     """
     find out if the image has a resolution larger than MIN_IMAGE_SIZE
     """
     if not image_url:
-        return None
-    image_pil = None
+        logging.error('Method malformed!')
+        return False
+
     try:
-        image_pil = Image.open(StringIO(urllib2.urlopen(image_url, timeout=UCK_TIMEOUT).read()))
+        # possible exception raiser
+        image_pil = Image.open(
+            StringIO(urllib2.urlopen(image_url, timeout=UCK_TIMEOUT).read()))
+
+        # to avoid line length limit
+        if image_pil.size[0] * image_pil.size[1] > MIN_IMAGE_SIZE[0] * MIN_IMAGE_SIZE[1]:
+            return True
+        else:
+            return False
     except Exception as k:
-        print '[thumbnail.is_valid_image]', str(k)
-        raise k
-    # to avoid line length limit
-    if image_pil.size[0] * image_pil.size[1] > MIN_IMAGE_SIZE[0] * MIN_IMAGE_SIZE[1]:
-        return True
-    else:
+        logging.exception(str(k))
         return False
 
 
-# TODO: boundary checkers
 # TODO: relative path could be a url including its suffix like jpg/png
 def generate_thumbnail(image_url, relative_path):
     """
-    docs needed!
+    generate a thumbnail
     """
     if not image_url or not relative_path:
+        logging.error('Method malformed!')
         return None
-    image_pil = None
+
     try:
-        image_pil = Image.open(StringIO(
-            urllib2.urlopen(image_url, timeout=UCK_TIMEOUT).read()))
+        # possible exception raiser
+        image_pil = Image.open(
+            StringIO(urllib2.urlopen(image_url, timeout=UCK_TIMEOUT).read()))
+
+        # generate thumbnail
+        if image_pil.size > MIN_IMAGE_SIZE:
+            # get various paths
+            image_thumbnail_local_path = '%s%si.jpg' % (
+                IMAGES_LOCAL_DIR, relative_path)
+            image_thumbnail_web_path = '%s%s.jpg' % (
+                IMAGES_PUBLIC_DIR, relative_path)
+
+            # thumbnailing
+            image_pil.thumbnail(MIN_IMAGE_SIZE, Image.ANTIALIAS)
+            image_pil = image_pil.convert('RGB')
+            image_pil.save(image_thumbnail_local_path, 'JPEG')
+            return image_thumbnail_web_path
+        else:
+            return image_url
     except Exception as k:
-        print '[thumbnail.generate_thumbnail]', str(k)
-        raise k
-    # generate thumbnail
-    if image_pil.size > MIN_IMAGE_SIZE:
-        image_thumbnail_local_path = '%s%si.jpg' % (
-            IMAGES_LOCAL_DIR, relative_path)
-        image_thumbnail_web_path = '%s%s.jpg' % (
-            IMAGES_PUBLIC_DIR, relative_path)
-        image_pil.thumbnail(MIN_IMAGE_SIZE, Image.ANTIALIAS)
-        image_pil = image_pil.convert('RGB')
-        image_pil.save(image_thumbnail_local_path, 'JPEG')
-        return image_thumbnail_web_path
-    else:
-        return image_url
+        logging.exception(str(k))
+        return None
 
 
 def get_image_size(image_url):
     """
     docs needed
     """
-    image_web = None
-    if isinstance(image_url, str) or isinstance(image_url, unicode):
-        print '[thumbnail.get_image_size] opening %s' % image_url 
-        image_web = StringIO(urllib2.urlopen(image_url, timeout=UCK_TIMEOUT).read())
-    else:
-        print '[thumbnail.get_image_size] image_url is data'
-        image_web = image_url
-    im = Image.open(image_web)
-    width, height = im.size
-    return width, height
+    if not image_url:
+        logging.error('Method malformed!')
+        return None, None
+
+    try:
+        image_web = None
+        if isinstance(image_url, str) or isinstance(image_url, unicode):
+            logging.info('opening %s' % image_url)
+            image_web = StringIO(
+                urllib2.urlopen(image_url, timeout=UCK_TIMEOUT).read())
+        else:
+            logging.info('image_url is data')
+            image_web = image_url
+
+        if image_web:
+            im = Image.open(image_web)
+            width, height = im.size
+            return width, height
+        else:
+            return None, None
+    except Exception as k:
+        logging.exception(str(k))
+        return None, None
