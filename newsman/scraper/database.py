08443f62b7d5427f642637452b199e088ebea9aa
chengdujin <chengdujin@gmail.com>
2013-08-27 00:24:27 +0800

    added logging to database.py

--- newsman/scraper/database.py
+++ newsman/scraper/database.py
@@ -14,8 +14,8 @@ reload(sys)
 sys.setdefaultencoding('UTF-8')
 sys.path.append('..')
 
-from config import Collection
-from config import db
+from config import Collection, db
+from config import logging
 
 # CONSTANTS
 from config import LANGUAGES
@@ -26,22 +26,29 @@ def dedup(entries=None, language=None):
     return entries not found in database
     """
     if not entries:
+        logging.error('Method malformed!')
         return None
     if not language or language not in LANGUAGES:
-        raise Exception("[database.dedup] ERROR: language not found or not supported!")
+        logging.exception("Language not found or not supported!")
+        return None
 
-    entries_new = []
-    col = Collection(db, language)
-    for entry in entries:
-        # find duplicate in the form of the same link or title
-        dup_link = col.find_one({'link': entry['link']})
-        dup_title = col.find_one({'title': entry['title']})
-        if dup_link or dup_title:
-            print '[database.dedup] Find a duplicate for %s' % str(entry['title'])
-            continue
-        else:
-            entries_new.append(entry)
-    return entries_new if entries_new else None
+    try:
+        entries_new = []
+        col = Collection(db, language)
+        for entry in entries:
+            # find duplicate in the form of the same link or title
+            dup_link = col.find_one({'link': entry['link']})
+            dup_title = col.find_one({'title': entry['title']})
+
+            if dup_link or dup_title:
+                logging.warning('Find a duplicate for %s' % str(entry['title']))
+                continue
+            else:
+                entries_new.append(entry)
+        return entries_new if entries_new else None
+    except Exception as k:
+        logging.exception(str(k))
+        return None
 
 
 def update(entry=None):
@@ -49,13 +56,19 @@ def update(entry=None):
     save an entry in database
     """
     if not entry:
+        logging.error('Method malformed!')
         return None
     if entry['language'] not in LANGUAGES:
-        raise Exception("[databse.update] ERROR: language not found or not supported!")
+        logging.exception("Language not found or not supported!")
+        return None
 
-    # collection was created by the feed
-    col = Collection(db, entry['language'])
-    # then save to database
-    entry_id = col.save(entry)
-    entry['_id'] = str(entry_id)
-    return entry
+    try:
+        # collection was created by the feed
+        col = Collection(db, entry['language'])
+        # then save to database
+        entry_id = col.save(entry)
+        entry['_id'] = str(entry_id)
+        return entry
+    except Exception as k:
+        logging.exception(str(k))
+        return None
