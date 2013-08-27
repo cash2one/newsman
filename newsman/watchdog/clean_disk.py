73dd77166c3091e3b30d8490ca5159fdf4575042
chengdujin <chengdujin@gmail.com>
2013-08-27 13:58:06 +0800

    added logging to clean_disk.py

--- newsman/watchdog/clean_disk.py
+++ newsman/watchdog/clean_disk.py
@@ -86,55 +86,60 @@ def _clean_unrecorded_files():
     5. remove the file if it is not found in database
     """
 
-    # "en": [(transcoded_local', '/home/work/xxx.html'), ('mp3_local':'/home/work/xxx.mp3')]
-    unrecorded_files = {}
-    # mp3 files
-    if os.path.exists(MEDIA_LOCAL_DIR):
-        media_files = [os.path.join(MEDIA_LOCAL_DIR, media_file) for media_file in os.listdir(MEDIA_LOCAL_DIR) if cleaner._is_overdue(os.path.getctime(os.path.join(MEDIA_LOCAL_DIR, media_file)))]
-        for media_file in media_files:
-            if os.path.exists(media_file):
-                document_name = media_file.split('_')[0]  # en, en-rIN, pt
-                if not unrecorded_files[document_name]:
-                    unrecorded_files[document_name] = []
-                unrecorded_files[document_name].append(('mp3_local', media_file))
+    try:
+        # "en": [(transcoded_local', '/home/work/xxx.html'), ('mp3_local':'/home/work/xxx.mp3')]
+        unrecorded_files = {}
+        # mp3 files
+        if os.path.exists(MEDIA_LOCAL_DIR):
+            media_files = [os.path.join(MEDIA_LOCAL_DIR, media_file) for media_file in os.listdir(MEDIA_LOCAL_DIR) if cleaner._is_overdue(os.path.getctime(os.path.join(MEDIA_LOCAL_DIR, media_file)))]
+            for media_file in media_files:
+                if os.path.exists(media_file):
+                    document_name = media_file.split('_')[0]  # en, en-rIN, pt
+                    if not unrecorded_files[document_name]:
+                        unrecorded_files[document_name] = []
+                    unrecorded_files[document_name].append(('mp3_local', media_file))
 
-    # image files
-    if os.path.exists(IMAGES_LOCAL_DIR):
-        image_files = [os.path.join(IMAGES_LOCAL_DIR, image_file) for image_file in os.listdir(IMAGES_LOCAL_DIR) if cleaner._is_overdue(os.path.getctime(os.path.join(IMAGES_LOCAL_DIR, image_file)))]
-        for image_file in image_files:
-            if os.path.exists(image_file):
-                document_name = image_file.split('_')[0]
-                image_type = '%s_image_local' % os.path.splitext(image_file)[0].split('_')[-1]  # category_image_local
-                if not unrecorded_files[document_name]:
-                    unrecorded_files[document_name] = []
-                unrecorded_files[document_name].append((image_type, image_file))
+        # image files
+        if os.path.exists(IMAGES_LOCAL_DIR):
+            image_files = [os.path.join(IMAGES_LOCAL_DIR, image_file) for image_file in os.listdir(IMAGES_LOCAL_DIR) if cleaner._is_overdue(os.path.getctime(os.path.join(IMAGES_LOCAL_DIR, image_file)))]
+            for image_file in image_files:
+                if os.path.exists(image_file):
+                    document_name = image_file.split('_')[0]
+                    image_type = '%s_image_local' % os.path.splitext(image_file)[0].split('_')[-1]  # category_image_local
+                    if not unrecorded_files[document_name]:
+                        unrecorded_files[document_name] = []
+                    unrecorded_files[document_name].append((image_type, image_file))
 
-    # transcoded files
-    if os.path.exists(TRANSCODED_LOCAL_DIR):
-        transcoded_files = [os.path.join(TRANSCODED_LOCAL_DIR, transcoded_file) for transcoded_file in os.listdir(TRANSCODED_LOCAL_DIR) if cleaner._is_overdue(os.path.getctime(os.path.join(TRANSCODED_LOCAL_DIR, transcoded_file)))]
-        for transcoded_file in transcoded_files:
-            if os.path.exists(transcoded_file):
-                document_name = transcoded_file.split('_')[0]
-                if not unrecorded_files[document_name]:
-                    unrecorded_files[document_name] = []
-                unrecorded_files[document_name].append(('transcoded_local', transcoded_file))
+        # transcoded files
+        if os.path.exists(TRANSCODED_LOCAL_DIR):
+            transcoded_files = [os.path.join(TRANSCODED_LOCAL_DIR, transcoded_file) for transcoded_file in os.listdir(TRANSCODED_LOCAL_DIR) if cleaner._is_overdue(os.path.getctime(os.path.join(TRANSCODED_LOCAL_DIR, transcoded_file)))]
+            for transcoded_file in transcoded_files:
+                if os.path.exists(transcoded_file):
+                    document_name = transcoded_file.split('_')[0]
+                    if not unrecorded_files[document_name]:
+                        unrecorded_files[document_name] = []
+                    unrecorded_files[document_name].append(('transcoded_local', transcoded_file))
 
-    # check and remove an unrecorded file
-    for document_name, items in unrecorded_files.iteritems():
-        # remove duplicated
-        items = list(set(items))
+        # check and remove an unrecorded file
+        for document_name, items in unrecorded_files.iteritems():
+            # remove duplicated
+            items = list(set(items))
 
-        # check if a file is still in databse
-        document = Collection(db, document_name)
-        for item in items:
-            field = item[0]
-            path = item[1]
-            found = document.find({field:path})
-            if not found:
-                # remove it if it were not
-                # check if file exists again for safety
-                if os.path.exists(path):
-                    os.remove(path)
+            # check if a file is still in databse
+            document = Collection(db, document_name)
+            for item in items:
+                field = item[0]
+                path = item[1]
+                found = document.find({field:path})
+                if not found:
+                    # remove it if it were not
+                    # check if file exists again for safety
+                    if os.path.exists(path):
+                        os.remove(path)
+        return True
+    except Exception as k:
+        logging.exception(str(k))
+        return False
 
 
 def clean():
