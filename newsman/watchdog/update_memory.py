039e355aefb966bfe2ca08c3ed5a1ed2e487e032
chengdujin <chengdujin@gmail.com>
2013-08-27 16:14:20 +0800

    finished a draft of update_memory.py

---
+++ newsman/watchdog/update_memory.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+sys.path.append("..")
+
+from config import Collection, db
+from config import rclient
+
+
+def _change_field(language):
+    """
+    change a key/field name in database and memory
+    """
+    # memory
+    id_total = rclient.zcard("news::%s" % language)
+    ids = rclient.zrange("news::%s" % language, 0, id_total)
+
+    for id in ids:
+        entry_string = rclient.get(id)
+        entry_string_new = entry_string.replace('hot_news_image', 'hotnews_image')
+        rclient.setex(id, rclient.ttl(id), entry_string_new)
+
+    # database
+    col = Collection(db, language)
+    col.update({}, {"rename": {"hot_news_image":"hotnews_image"}}, False, True)
+    col.update({}, {"rename": {"hot_news_image_local":"hotnews_image_local"}}, False, True)
+
+
+if __name__ == '__main__':
+    language = sys.argv[1]
+    _change_field(language)
+
