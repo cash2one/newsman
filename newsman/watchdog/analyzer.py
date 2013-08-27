2b0b044eaad3f82601eee1e4a32acdd1f22c3390
chengdujin <chengdujin@gmail.com>
2013-08-27 14:03:34 +0800

    changed a path

--- newsman/watchdog/analyzer.py
+++ newsman/watchdog/analyzer.py
@@ -12,7 +12,7 @@ scrape is a task to scrape rss resources
 import sys
 reload(sys)
 sys.setdefaultencoding('UTF-8')
-sys.path.append('/home/jinyuan/Downloads/newsman')
+sys.path.append('/home/jinyuan/Downloads/newsman/newsman')
 
 from config import Collection, db
 from config import logging
