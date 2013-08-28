3a4089f205f9b05a6bde69a9f869545188647a47
chengdujin <chengdujin@gmail.com>
2013-08-28 20:50:27 +0800

    autopep8ed clean_database.py

--- newsman/watchdog/clean_database.py
+++ newsman/watchdog/clean_database.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python 
-#-*- coding: utf-8 -*- 
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 
 """
 daily work, clean expired items and its place in queues in memory
@@ -9,13 +9,14 @@ daily work, clean expired items and its place in queues in memory
 # @created Aug. 22, 2013
 
 
-import sys 
-reload(sys) 
+import sys
+reload(sys)
 sys.setdefaultencoding('UTF-8')
 sys.path.append("..")
 
 import calendar
-import clean_disk, clean_memory
+import clean_disk
+import clean_memory
 from config import Collection, db
 from config import logger
 from datetime import datetime, timedelta
@@ -35,7 +36,7 @@ def _find_document_names():
         feeds = Collection(db, FEED_REGISTRAR)
         document_names = []
         # only get language and _id
-        items = feeds.find({}, {'language':1})
+        items = feeds.find({}, {'language': 1})
         if items:
             document_names = [item['language'] for item in items]
         return list(set(document_names))
@@ -57,10 +58,12 @@ def clean():
 
                 # compute a threshold
                 current_utc_time_posix = calendar.timegm(time.gmtime())
-                deadline_datetime = datetime.utcfromtimestamp(current_utc_time_posix) - timedelta(days=DATABASE_REMOVAL_DAYS)
+                deadline_datetime = datetime.utcfromtimestamp(
+                    current_utc_time_posix) - timedelta(days=DATABASE_REMOVAL_DAYS)
                 deadline_posix = calendar.timegm(deadline_datetime.timetuple())
 
-                removal_candidates = document.find({'updated': {'$lt': deadline_posix}})
+                removal_candidates = document.find(
+                    {'updated': {'$lt': deadline_posix}})
                 for removal_candidate in removal_candidates:
                     # see if removal candidate has a footage in memory
                     clean_memory.clean_by_item(str(removal_candidate['_id']))
