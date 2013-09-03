3ba4c21ea8652dc51c8471605aa94f0b73a6c5cd
chengdujin <chengdujin@gmail.com>
2013-09-03 17:59:54 +0800

    finished a draft of idler.py

--- newsman/watchdog/cron/idler.py
+++ newsman/watchdog/cron/idler.py
@@ -34,11 +34,33 @@ def _find_idler():
         if feeds:
             for feed in feeds:
                 if 'latest_update' in feed and feed['latest_update']:
+                    # read string value from database and convert it into unix time
                     latest_update_time = time.mktime(time.strptime(str(feed['latest_update'])))
+                    # change unix time into datetime struct
                     latest_update_datetime = datetime.utcfromtimestamp(latest_update_time)
-                    checkpoint_datetime = timedelta(days=)
-                else:
-                    pass
+                    # convert feed_update_days into datetime struct
+                    checkpoint_datetime = timedelta(days=FEED_UPDATE_DAYS)
+                    # compute feed update deadline in datetime struct
+                    feed_deadline_datetime = latest_update_datetime + checkpoint_datetime
+                    # convert feed update deadline back to unix time
+                    feed_deadline_time = time.mktime(feed_deadline_datetime.utctimetuple())
+
+                    # compute time now in unix
+                    now_time = time.mktime(time.gmtime())
+
+                    # compare feed_deadline with now
+                    if feed_deadline_time > now_time: # feed was updated correctly
+                        pass
+                    else:
+                        if 'feed_title' in feed and feed['feed_title']:
+                            logger.error('%s (%s) has not been updated for 2 days!' % (feed['feed_title'], feed['feed_link']))
+                        else:
+                            logger.error('%s has not been updated for 2 days!' % feed['feed_link'])
+                else:  # nothing found in feed about latest_update
+                    if 'feed_title' in feed and feed['feed_title']:
+                        logger.error('%s (%s) has never been updated!' % (feed['feed_title'], feed['feed_link']))
+                    else:
+                        logger.error('%s has never been updated!' % feed['feed_link'])
         else:
             logger.error('Cannot find any feed in %s' % FEED_REGISTRAR)
     except Exception as k:
