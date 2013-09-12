bc3b609325b89d8e47cfaa632682da2367acb7fa
chengdujin <chengdujin@gmail.com>
2013-09-12 16:07:32 +0800

    Update restore.....py

--- newsman/bin/restore_memory_from_database.py
+++ newsman/bin/restore_memory_from_database.py
@@ -17,48 +17,53 @@ sys.path.append("..")
 from bson.objectid import ObjectId
 from config.settings import Collection, db
 from config.settings import rclient
+from datetime import datetime, timedelta
 from scraper import memory
 
 # CONSTANTS
 from config.settings import MEMORY_RESTORATION_DAYS
+from config.settings import LANGUAGES
+
+
+def _get_expiration(updated):
+    """
+    Compute expiration time
+    """
+    if not updated:
+        return 0
+
+    deadline = datetime.utcfromtimestamp(updated) + timedelta(days=MEMORY_RESTORATION_DAYS)
+    time_left = deadline - datetime.now()
+    return time_left.days * 60 * 60 * 24 + time_left.seconds
 
 
 def restore():
-    '''if memory failed, restore items from database'''
-    def get_expiration(updated):
-        ''''''
-        deadline = datetime.utcfromtimestamp(
-            updated) + timedelta(days=MEMORY_RESTORATION_DAYS)
-        time_left = deadline - datetime.now()
-        return time_left.days * 60 * 60 * 24 + time_left.seconds
+    """
+    if memory failed, restore items from database
+    """
+    # check if redis is alive
+    try:
+        rcling.ping()
+    except ConnectionError:
+        print 'Redis is down! Cannot recover memory data from database'
+        return None
 
-    print '----------------------restoring-------------------------'
-    language_files = read_task_directory()
-    for language_file in language_files:
-        print 'database to memory'
-        l = open(language_file, 'r')
-        lines = l.readlines()
-        language = None
-        col = None
-        for line in lines:
-            language, category, feed_id, feed_link = extract_task(line)
+    print '=== RESTORE MEMORY FROM DATABASE ==='
+    collections = db.collection_names()
+    for collection in collections:
+        if collection != 'system.indexes' and collection != 'feeds':
+            language = collection
             col = Collection(db, language)
+
+            # find valid time to filter out expired items
             current_utc_time_posix = calendar.timegm(time.gmtime())
-            active_datetime = datetime.utcfromtimestamp(
-                current_utc_time_posix) - timedelta(days=MEMORY_RESTORATION_DAYS)
+            active_datetime = datetime.utcfromtimestamp(current_utc_time_posix) - timedelta(days=MEMORY_RESTORATION_DAYS)
             active_posix = calendar.timegm(active_datetime.timetuple())
-            items = col.find(
-                {'category': category, 'feed': feed_id, 'updated': {'$gte': active_posix}}).sort('updated', -1)
+
+            items = col.find({'updated': {'$gte': active_posix}}).sort('updated', -1)
             if items:
-                items_with_expiration = [(item, get_expiration(float(item['updated'])))
-                                         for item in items]
-                # for i in items_with_expiration:
-                    #ent = i[0]
-                    #exp = i[1]
-                    # print '++', ent['_id'],
-                    # datetime.utcfromtimestamp(ent['updated']), exp
-                memory.update_memory(
-                    items_with_expiration, language, category, feed_id)
+                items_with_expiration = [(item, _get_expiration(float(item['updated']))) for item in items]
+                memory.update_memory(items_with_expiration, language, category, feed_id)
             print language, category, feed_id, len(items_with_expiration)
         l.close()
         print 'memory to database for %s' % language
