ab847780dcde3dbc62f8a13956dca6438b65adfd
chengdujin <chengdujin@gmail.com>
2013-09-23 16:38:40 +0800

    Create manager.py in feed_manager

---
+++ newsman/feed_manager/manager.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+"""
+Feed management supports operations like Insert, Modify and Remove on the 
+level of an RSS feed, a label and a category
+"""
+# @author chengdujin
+# @contact chengdujin@gmail.com
+# @created Sept. 23, 2013
+
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+
+
+if __name__ == '__main__':
+    pass
