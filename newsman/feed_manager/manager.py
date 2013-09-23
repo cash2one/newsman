8e8cb2567bf3c304eb77dfc443a6829808334dad
chengdujin <chengdujin@gmail.com>
2013-09-23 18:00:01 +0800

    Update manager.py

--- newsman/feed_manager/manager.py
+++ newsman/feed_manager/manager.py
@@ -13,6 +13,27 @@ level of an RSS feed, a label and a category
 import sys 
 reload(sys) 
 sys.setdefaultencoding('UTF-8')
+sys.path.append('..')
+
+from config.settings import Collection, db
+from config.settings import rclient
+
+# CONSTANTS
+from config.settings import FEED_REGISTRAR
+
+
+def update_label(language=None, label=None):
+    """
+    Update memory and database on label change
+    Note. label should be country::category::label-like
+    """
+
+    if not language or not label:
+        return None
+
+    label_name_in_memory = 'news::%s::%s' % (language, label)
+    feeds = Collection(db, FEED_REGISTRAR) 
+    items = feeds.find({'labels':label}, {'feed_title'})
 
 
 if __name__ == '__main__':
