915f2b33eaa0c0dfe16a9adec454674a1e43e7fc
chengdujin <chengdujin@gmail.com>
2013-10-29 13:26:03 +0800

    Update logo_keeper.py

--- newsman/bin/feed_logos/logo_keeper.py
+++ newsman/bin/feed_logos/logo_keeper.py
@@ -16,11 +16,16 @@ sys.path.append('../..')
 
 from config.settings import Collection
 from config.settings import db
+import os
 
 # CONSTANTS
 from config.settings import FEED_REGISTRAR
-#FILE_PREFIX = '/home/work/newsman/newsman/bin/text_based_feeds/feed_lists/'
-#FILE_PREFIX = '/home/ubuntu/newsman/newsman/bin/text_based_feeds/feed_lists/'
+#LOGOS_PREFIX = '/home/work/Downloads/newsman/newsman/bin/feed_logos/logos/'
+#LOGOS_PREFIX = '/home/ubuntu/Downloads/newsman/newsman/bin/feed_logos/logos/'
+LOGOS_PREFIX = '/home/jinyuan/Downloads/newsman/newsman/bin/feed_logos/logos/'
+
+#FILE_PREFIX = '/home/work/Downloads/newsman/newsman/bin/text_based_feeds/feed_lists/'
+#FILE_PREFIX = '/home/ubuntu/Downloads/newsman/newsman/bin/text_based_feeds/feed_lists/'
 FILE_PREFIX = '/home/jinyuan/Downloads/newsman/newsman/bin/text_based_feeds/feed_lists/'
 
 
@@ -47,59 +52,29 @@ def _convert(language='en', country=None):
     turn text-based feed infor into database items
     Note. 1. categories: [(), ()]
     """
+    logo_list = os.listdir('%s%s_%s' % (LOGOS_PREFIX, language, country))
+
     # read in file content
     feeds_list = open('%s%s_%s_feeds_list' % (FILE_PREFIX, language, country), 'r')
     lines = feeds_list.readlines()
     feeds_list.close()
 
-    # open datbase
-    db_feeds = Collection(db, FEED_REGISTRAR)
-    db_id_list = open('db_id_list', 'a')
+    new_feeds_list = open('%s%s_%s_feeds_list_new' % (FILE_PREFIX, language, country), 'w')
 
     for line in lines:
         if line.strip():
-            language, category, transcoder, feed_link, feed_title, labels = _parse_task(line)
-            if feed_link:
-                category = '%s::%s' % (country, category)
-
-                # break labels
-                if labels:
-                    labels = ['%s::%s' % (category, label.strip()) for label in labels.split(',')]
-
-                print feed_link
-
-                existing_item = db_feeds.find_one({'feed_link':feed_link})
-                if not existing_item:
-                    _id = db_feeds.save({'language': language, 'countries':[country], 'feed_link': feed_link, 'categories': [category], 'labels':labels, 'feed_title': feed_title, 'latest_update': None, 'updated_times': 0, 'transcoder': transcoder})
-                    db_id_list.write(str(_id) + '\n')
-                else:
-                    new_item = existing_item
-                    new_item['language'] = language
-                    
-                    if 'categories' in existing_item and existing_item['categories'] and category:
-                        existing_item['categories'].append(category)
-                        new_item['categories'] = list(set(existing_item['categories']))
+            language, category, transcoder, link, title, labels = _parse_task(line)
+            for logo in logo_list:
+                logo_clean = logo.replace('.png', "")
+                if logo_clean in link:
+                    logo_path = "%s%_%s/%s" % (LOGOS_PREFIX, language, country, logo)
+                    if labels:
+                        new_feeds_list.write('%s*|*%s*|*%s*|*%s*|*%s*|*%s*|*%s\n' % (language, category, transcoder, link, name_dict[link], logo_path, labels))
                     else:
-                        new_item['categories'] = [category]
+                        new_feeds_list.write('%s*|*%s*|*%s*|*%s*|*%s*|*%s\n' % (language, category, transcoder, link, name_dict[link], logo_path))
+                    break
 
-                    if 'labels' in existing_item and existing_item['labels'] and labels:
-                        existing_item['labels'].extend(labels)
-                        new_item['labels'] = list(set(existing_item['labels']))
-                    else:
-                        new_item['labels'] = labels
-
-                    existing_item['countries'].extend([country])
-                    new_item['countries'] = list(set(existing_item['countries']))
-
-                    new_item['transcoder'] = transcoder
-                    new_item['feed_title'] = feed_title
-                    db_feeds.update({'_id': existing_item['_id']}, new_item)
-                    db_id_list.write(str(existing_item['_id']) + '\n')
-            else:
-                continue
-        else:
-            continue
-    db_id_list.close()
+    new_feeds_list.close()
 
 
 if __name__ == "__main__":
