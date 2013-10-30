047a075672275a4170cde3743d4991f944c1a64e
chengdujin <chengdujin@gmail.com>
2013-10-30 11:57:48 +0800

    Update print_feeds.py

--- newsman/bin/text_based_feeds/print_feeds.py
+++ newsman/bin/text_based_feeds/print_feeds.py
@@ -10,26 +10,59 @@ from config.settings import Collection, db
 import feedparser
 
 
-col = Collection(db, 'feeds')
+def _parse_task(line):
+    """
+    read *_feeds_list.txt
+    """
+    line = line.strip()
+    if line:
+        task = line.strip().split('*|*')
+        # task[1] refers to categories
+        return task[0].strip(), task[1].strip(), task[2].strip(), task[3].strip()
+    else:
+        return None
 
-output = {}
-items = col.find()
-for item in items:
-    for country in item['countries']:
-        country = str(country)
-        if country not in output:
-            output[country] = []
-        ss = feedparser.parse(item['feed_link'])
-        print item['feed_link']
-        feed_title = ss['feed']['title'] if ss and 'title' in ss['feed'] else item['feed_link']
-        print str(feed_title)
-        output[country].append('%s  %s' % (item['categories'][0], str(feed_title)))
-        print
+def _find(language, country):
+    """
+    """
+    feeds_list = open('%s%s_%s_feeds_list' % (FILE_PREFIX, language, country), 'r')
+    lines = feeds_list.readlines()
+    feeds_list.close()
+    
+    for line in lines:
+        language, category, transcoder, link = _parse_task(line)
+        ss = feedparser.parse(link)
+        feed_title = ss['feed']['title'] if ss and 'title' in ss['feed'] else None
+        print "[%s]" % feed_title, link
 
-f = open('test', 'w')
-for k, v in output.iteritems():
-    f.write(k + '\n')
-    for i in v:
-        f.write("    " + i + '\n')
-    f.write('\n')
-f.close()
+    """
+    col = Collection(db, 'feeds')
+
+    output = {}
+    items = col.find()
+    for item in items:
+        for country in item['countries']:
+            country = str(country)
+            if country not in output:
+                output[country] = []
+            ss = feedparser.parse(item['feed_link'])
+            print item['feed_link']
+            feed_title = ss['feed']['title'] if ss and 'title' in ss['feed'] else item['feed_link']
+            print str(feed_title)
+            output[country].append('%s  %s' % (item['categories'][0], str(feed_title)))
+            print
+
+    f = open('test', 'w')
+    for k, v in output.iteritems():
+        f.write(k + '\n')
+        for i in v:
+            f.write("    " + i + '\n')
+        f.write('\n')
+    f.close()
+    """
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        _find(sys.argv[1], sys.argv[2])
+    else:
+        print 'Please indicate a language and country'
