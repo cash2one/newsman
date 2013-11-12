0617c5fccf9a90bc511366f945ed4b1de981a544
chengdujin <chengdujin@gmail.com>
2013-11-12 19:57:27 +0800

    Complete a draft of _clean_article

--- newsman/data_processor/pyteaser.py
+++ newsman/data_processor/pyteaser.py
@@ -29,10 +29,87 @@ class PyTeaser:
     PyTeaser extracts key sentences from an article
     """
 
-    def _score_sentences(article=None, title=None, topwords=None):
+    def __init__(self, language=None, title=None, article=None, link=None, blog=None, category=None):
+        if not language or not title or not article:
+            logger.error('Method malformed!')
+
+        self.language = language
+        self.title = title
+        self.article = article
+        self.link = link
+        self.blog = blog
+        self.category = category
+
+
+    def summarize(self):
         """
+        summarize is the entry to summarization
+        3. segement title
+        4. compute keywords of the article
+        5. find the 'top' keywords
+        6. compute score of each sentence
+        7. output the first x sentences in ranking
         """
-        if not article or not title or not topwords:
+        # 'clean' the article
+        _clean_article()
+
+        # split article into sentences
+        _split_article()
+
+
+    def _clean_article(self):
+        """
+        remove html tags, images, links from the article, and encode it appropriately
+        """
+        try:
+            # convert to normal encoding
+            self.article = str(urllib2.unquote(hparser.unescape(self.article)))
+
+            # remove unnecessary parts
+            html_stripper = html2text.HTML2Text()
+            html_stripper.ignore_links = True
+            html_stripper.ignore_images = True
+            html_stripper.ignore_emphasis = True
+            self.article = html_stripper.handle(self.article).strip("#")
+
+            # convert to appropriate encoding
+            self.article = self.article.decode(chardet.detect(self.article)['encoding'], 'ignore')
+        except Exception as k:
+            logger.error(str(k))
+            return None
+
+
+    def _split_article(self):
+        """
+        use nltk or other engines to split the article into sentences
+        """
+        try:
+            pass
+        except Exception as k:
+            logger.error(str(k))
+            return None
+
+
+    def _segment_text(self, text=None):
+        """
+        segment thext into words
+        """
+        if not text:
+            logger.error("Method malformed!")
+            return None
+
+        try:
+            pass
+        except Exception as k:
+            logger.error(str(k))
+            return None
+
+
+    def _find_keywords(self, article=None):
+        """
+        compute word-frenquecy map
+        """
+        if not article:
             logger.error("Method malformed!")
             return None
 
@@ -58,11 +135,10 @@ class PyTeaser:
             return None
 
 
-    def _find_keywords(article=None):
+    def _score_sentences(article=None, title=None, topwords=None):
         """
-        compute word-frenquecy map
         """
-        if not article:
+        if not article or not title or not topwords:
             logger.error("Method malformed!")
             return None
 
@@ -71,84 +147,6 @@ class PyTeaser:
         except Exception as k:
             logger.error(str(k))
             return None
-
-
-    def _segment_text(text=None):
-        """
-        segment thext into words
-        """
-        if not text:
-            logger.error("Method malformed!")
-            return None
-
-        try:
-            pass
-        except Exception as k:
-            logger.error(str(k))
-            return None
-
-
-    def _split_article(article=None):
-        """
-        use nltk or other engines to split the article into sentences
-        """
-        if not article:
-            logger.error("Method malformed!")
-            return None
-
-        try:
-            pass
-        except Exception as k:
-            logger.error(str(k))
-            return None
-
-
-    def _clean_article(article=None):
-        """
-        remove html tags, images, links from the article, and encode it appropriately
-        """
-        if not article:
-            logger.error("Method malformed!")
-            return None
-
-        try:
-            # convert to normal encoding
-            article = str(urllib2.unquote(hparser.unescape(article)))
-
-            # remove unnecessary parts
-            html_stripper = html2text.HTML2Text()
-            html_stripper.ignore_links = True
-            html_stripper.ignore_images = True
-            html_stripper.ignore_emphasis = True
-            clean_article = html_stripper.handle(article).strip("#")
-
-            # convert to appropriate encoding
-        except Exception as k:
-            logger.error(str(k))
-            return None
-
-
-    def summarize(language=None, title=None, article=None, link=None, blog=None, category=None):
-        """
-        summarize is the entry to summarization. it does the following:
-        1. 'clean' the article
-        2. split article into sentences
-        3. segement title
-        4. compute keywords of the article
-        5. find the 'top' keywords
-        6. compute score of each sentence
-        7. output the first x sentences in ranking
-        """
-        pass
-
-
-    def __init__(self, language=None, title=None, article=None, link=None, blog=None, category=None):
-        self.language = language
-        self.title = title
-        self.article = article
-        self.link = link
-        self.blog = blog
-        self.category = category
 
 
 if __name__ == '__main__':
