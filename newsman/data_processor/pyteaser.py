20024abc05de5a16207e5891fab73f3fe802a34e
chengdujin <chengdujin@gmail.com>
2013-11-12 17:58:18 +0800

    Add pyteaser.py

---
+++ newsman/data_processor/pyteaser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*- 
+
+"""
+PyTeaser is a Python version of [TextTeaser]<https://github.com/MojoJolo/textteaser/>
+PyTeaser uses nltp instead of Scala/Java's OpenNLP as the NLP engine. It also
+manages problems from internationalization, languages supported including Thai, 
+Arabic, Chinese, Japanese, Portugues and Indonesian
+"""
+# @author chengdujin
+# @contact chengdujin@gmail.com
+# @created Nov. 12, 2013
+
+
+import sys 
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+
