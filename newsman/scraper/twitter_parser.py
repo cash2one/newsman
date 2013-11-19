6c2cb8d425b1f898e8fe0e27bbf4049e689970bd
chengdujin <chengdujin@gmail.com>
2013-11-19 18:17:55 +0800

    Update twitter_parser code

--- newsman/scraper/twitter_parser.py
+++ newsman/scraper/twitter_parser.py
@@ -13,21 +13,39 @@ import sys
 reload(sys) 
 sys.setdefaultencoding('UTF-8')
 
-from config.settings import ACCESS_TOKEN_KEY
-from config.settings import ACCESS_TOKEN_SECRET
-from config.settings import consumer_key
-from config.settings import consumer_secret
+from config.settings import logger
+from config.settings import TWITTER_ACCESS_TOKEN_KEY
+from config.settings import TWITTER_ACCESS_TOKEN_SECRET
+from config.settings import TWITTER_CONSUMER_KEY
+from config.settings import TWITTER_CONSUMER_SECRET
 import twitter
 import urllib2
 
-
+# twitter api interface
 api = twitter.Api(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
-def parse(screen_name, feed_id, feed_title, language, categories):
+
+def _read_entry(status):
+    """
+    scrape information related to Twitter tweet
+    """
+    if not status:
+        logger.error("Method malformed!")
+
+
+def parse(screen_name, feed_id, feed_title, language, categories, etag):
     """
     Connect twitter and parses the timeline
     """
-    pass
+    count = 200
+    # since_id is the latest stored tweet id
+    # count limits the number of replies, maxium 200
+    statuses = api.GetUserTimeline(screen_name, since_id=etag, count=count)
+    entries = []
+    for s in statuses:
+        entry = _read_entry()
+        if entry:
+            entries.append(entry)
 
 
 if __name__ == "__main__":
