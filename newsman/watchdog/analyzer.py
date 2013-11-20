fab43476b0821a83758a7f1773377a838e0e15f7
chengdujin <chengdujin@gmail.com>
2013-11-20 16:28:43 +0800

    Changed from rss to spider

--- newsman/watchdog/analyzer.py
+++ newsman/watchdog/analyzer.py
@@ -19,7 +19,7 @@ sys.setdefaultencoding('UTF-8')
 
 from config.settings import Collection, db
 from config.settings import logger
-from scraper import rss
+from scraper import spider
 
 # CONSTANTS
 from config.settings import FEED_REGISTRAR
@@ -35,7 +35,7 @@ def _update(feed_ids):
 
     try:
         for feed_id in feed_ids:
-            updated = rss.update(feed_id=feed_id)
+            updated = spider.update(feed_id=feed_id)
             if not updated:
                 logger.info('Nothing got updated from %s' % feed_id)
             else:
