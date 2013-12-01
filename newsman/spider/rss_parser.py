26143f9f85a8ddfef34c5092f498b6eaec2280a5
chengdujin <chengdujin@gmail.com>
2013-12-02 01:01:27 +0800

    Fix a typo

--- newsman/spider/rss_parser.py
+++ newsman/spider/rss_parser.py
@@ -112,7 +112,7 @@ def _read_entry(e=None, feed_id=None, feed_title=None, language=None, categories
                     entry['link'] = original_link
                 """
                 original_link = urllib2.unquote(hparser.unescape(original_link))
-                f = urlib2.urlopen(original_link)
+                f = urllib2.urlopen(original_link)
                 entry['link'] = f.geturl()
             else:
                 logger.info('Advertising link %s' % original_link)
