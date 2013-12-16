588add1db74b0f18f6bbd1ed81ea58872285ddd2
chengdujin <chengdujin@gmail.com>
2013-12-16 19:03:33 +0800

    Update test code

--- newsman/test/test_rss.py
+++ newsman/test/test_rss.py
@@ -8,13 +8,6 @@ sys.path.append('..')
 
 from spider import scraper
 
-#entries = scraper.update(feed_link='http://news.yahoo.com/rss/world', feed_id='520b7da3680ccf3c10e93d55', language='en')
-entries = scraper.update(feed_id='5296e329f5d7e61827936108', feed_link='http://men.kapook.com/technology/feed', language='th')
-#entries = scraper.update(feed_link='http://www.mtv.com/rss/news/news_full.jhtml', feed_id='520b7da3680ccf3c10e93d6e', language='en')
-#entries = scraper.update(feed_link='http://feeds.reuters.com/reuters/healthNews', feed_id='520b7da3680ccf3c10e93d4b', language='en')
-
-#entries = scraper.update(feed_link='http://www.antaranews.com/rss/nasional-kesehatan', feed_id='008', language='ind', categories=categories)
-
-#entries = scraper.update(feed_link='http://sankei.jp.msn.com/rss/news/politics.xml', feed_id='5203d468680ccf6430d7ab7a', language='ja')
-#entries = scraper.update(feed_link='http://news.goo.ne.jp/rss/topstories/world/index.rdf', feed_id='5203d468680ccf6430d7ab7d', language='ja', categories=categories)
-#entries = scraper.update(feed_link='http://community.travel.yahoo.co.jp/domestic/blog.html?m=rss', feed_id='5203d468680ccf6430d7ab75', language='ja')
+#entries = scraper.update(feed_id='5257d25509d1f72ff1aa8abc', feed_link='http://hilight.kapook.com/main/feed', language='th')
+#entries = scraper.update(feed_id='5257d25509d1f72ff1aa8abe', feed_link='http://rssfeeds.sanook.com/rss/feeds/sanook/news.index.xml', language='th')
+entries = scraper.update(feed_id='5257d25509d1f72ff1aa8ac3', feed_link='http://news.mthai.com/category/world-news/feed', language='th')
