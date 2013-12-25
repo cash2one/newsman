a8f0cb49b13cc3b4de6934a9543cc94aa3f31f0b
chengdujin <chengdujin@gmail.com>
2013-12-25 14:01:11 +0800

    Change spider import to parser in test

--- newsman/test/test_rss_parser.py
+++ newsman/test/test_rss_parser.py
@@ -6,7 +6,7 @@ reload(sys)
 sys.setdefaultencoding('UTF-8')
 sys.path.append('..')
 
-from spider import rss_parser
+from parser import rss_parser
 
 #entries = rss_parser.parse(feed_link='http://news.yahoo.com/rss/us', language='en')
 #entries = rss_parser.parse(feed_link='http://www.engadget.com/rss.xml', language='en')
