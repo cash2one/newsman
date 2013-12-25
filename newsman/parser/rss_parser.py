bc527fb743878849720c6280623207debcd61dd3
chengdujin <chengdujin@gmail.com>
2013-12-25 14:12:34 +0800

    Changed http code 304 logger level from error to warning

--- newsman/parser/rss_parser.py
+++ newsman/parser/rss_parser.py
@@ -280,7 +280,7 @@ def parse(feed_link=None, feed_id=None, feed_title=None, language=None, categori
                     '%s has been permantently moved to a %s!' % (feed_link, d.href))
                 return None, status, feed_title, etag, modified, '%s has been permantently moved to a %s!' % (feed_link, d.href)
             elif status == 304:
-                logger.error('%s server has not updated its feeds' % feed_link)
+                logger.warning('%s server has not updated its feeds' % feed_link)
                 return None, status, feed_title, etag, modified, '%s server has not updated its feeds' % feed_link
             elif status == 410:
                 logger.critical(
