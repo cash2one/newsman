086281e93c1868418f8845aa9366c75319b0e8c7
chengdujin <chengdujin@gmail.com>
2014-01-14 18:43:21 +0800

    Comment out a test case

--- newsman/test/test_rss_parser.py
+++ newsman/test/test_rss_parser.py
@@ -11,7 +11,7 @@ from analyzer import rss_parser
 #entries = rss_parser.parse(feed_link='http://news.yahoo.com/rss/us', language='en')
 #entries = rss_parser.parse(feed_link='http://www.engadget.com/rss.xml', language='en')
 #entries = rss_parser.parse(feed_link='http://rss.cnn.com/rss/edition_sport.rss', language='en')
-entries = rss_parser.parse(feed_link='http://news.yahoo.com/rss/sports', language='en')
+#entries = rss_parser.parse(feed_link='http://news.yahoo.com/rss/sports', language='en')
 entries = rss_parser.parse(feed_link='http://rss.detik.com/index.php/sport', feed_id="5264f7cb0ff6cb1898609028", language='in', categories={"ID::Olahraga" : "1"})
 print len(entries)
 print entries[0]
